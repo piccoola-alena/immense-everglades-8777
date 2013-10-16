ec21fc9022aa571f55983f7ccc5ba1e7418971ca
piccoola-alena <piccoola3@mail.ru>
2013-10-16 22:42:23 +0400

    4

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+
+require '/src/facebook.php';
+
+// Create our Application instance (replace this with your appId and secret).
+$facebook = new Facebook(array(
+  'appId' => '1400569390162590',
+  'secret' => '905279329d9655c570a3f35e14b0113a',
+));
+
   echo "Hello World";
 ?>
