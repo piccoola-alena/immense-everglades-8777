9dc6f0d2b46191914f6fdbd512e93bf79d1d59fb
piccoola-alena <piccoola3@mail.ru>
2013-10-16 23:56:01 +0400

    j4+

--- index.php
+++ index.php
@@ -9,3 +9,28 @@ echo "Hello World";
 ?>
 
 <script src="http://connect.facebook.net/en_US/all.js"></script>
+
+
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    // init the FB JS SDK
+    FB.init({
+      appId      : '1400569390162590',                        // App ID from the app dashboard
+      channelUrl : '//http://immense-everglades-8777.herokuapp.com//channel.html', // Channel file for x-domain comms
+      status     : true,                                 // Check Facebook Login status
+      xfbml      : true                                  // Look for social plugins on the page
+    });
+
+    // Additional initialization code such as adding Event Listeners goes here
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
