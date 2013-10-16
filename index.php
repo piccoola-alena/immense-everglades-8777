b0d152fcc6b0f2069ae4cc11adec0cfaf30ca240
piccoola-alena <piccoola3@mail.ru>
2013-10-16 23:48:10 +0400

    jj3

--- index.php
+++ index.php
@@ -1,17 +1,10 @@
 <?php
 
-
-
-
-
-  echo "Hello World";
+echo "Hello World";
 
 <script src="http://connect.facebook.net/en_US/all.js"></script>
-  echo "Hello World";
- 
-</script>
-
-  echo "Hello World";
+echo "Hello World";
+echo "Hello World";
 
 
 ?>
