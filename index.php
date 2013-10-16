4259dcb2996c5db4a741cde473d9047eebd2080b
piccoola-alena <piccoola3@mail.ru>
2013-10-16 12:43:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,92 +1,99 @@
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>php-sdk</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Whats on our Mind</title>
-<link rel="stylesheet" type="text/css" href="src/view.css" media="all">
-<script type="text/javascript" src="src/view.js"></script>
-  </head>
-  <body id="main_body">
 <?php
 
-require '/src/facebook.php';
+  include_once "facebook.php";
 
-// Create our Application instance (replace this with your appId and secret).
-$facebook = new Facebook(array(
-  'appId'  => '1400569390162590',
-  'secret' => '905279329d9655c570a3f35e14b0113a',
-));
+  $app_id = '1400569390162590'; //  Update your app id
+  $app_secret = '905279329d9655c570a3f35e14b0113a'; //  Update your app id
+  $my_url = 'http://immense-everglades-8777.herokuapp.com//index.php' // Give absolute path of current file
 
-// Get User ID
-$user = $facebook->getUser();
+  $code = $_REQUEST["code"];
+  
+  
+  	function db_insert($insert_query)
+		{
+		// Instantiate the mysqli class
+		$mysqli = new mysqli();
+		
 
-// We may or may not have this data based on whether the user is logged in.
-//
-// If we have a $user id here, it means we know the user is logged into
-// Facebook, but we don't know if the access token is valid. An access
-// token is invalid if the user logged out of Facebook.
+//		Provide your DB credentials here
 
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
+		// Connect to the database server and select a database
+		$mysqli->connect('your db server', 'your db id', 'your password', 'your db name');
 	
-  } catch (FacebookApiException $e) {
-    error_log($e);
-    $user = null;
-  }
-}
+		//Execute query 
 
-// Login or logout url will be needed depending on current user state.
-if ($user) {
-  $logoutUrl = $facebook->getLogoutUrl();
-} else {
- //redirect user to authorize dialog
-   $login_url = $facebook->getLoginUrl($params = array('scope' => "publish_stream"));
+		$result = $mysqli->query($insert_query);
 
-    echo ("<script> top.location.href='".$login_url."'</script>");
-}
-
+		//commit
+		$commit = $mysqli->commit();
+	
+	    	// Close the connection
+    		$mysqli->close();    		
+ 		
+		}
 
 ?>
-<img id="top" src="images/top.png" alt="">
-	<div id="form_container">
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>Purple BI: Facebook Data Extraction & Analytics </title>
+</head> 
+<body> 
+	<h2>Facebook Data Extraction using FQL</h2> 
 	
-		<h1><a>Whats on our Mind</a></h1>
-		<form id="form_545340" class="appnitro"  method="post" action="process.php">
-					<div class="form_description">
-			<h2>Whats on our Mind</h2>
-			<p>Update your facebook status easily.</p>
-		</div>						
-			<ul >
-			
-					<li id="li_1" >
-		<label class="description" for="element_1"> </label>
-		<div>
-			<textarea id="txtstatus" name="txtstatus" class="element textarea small"></textarea> 
-		</div> 
-		</li>
-			
-					<li class="buttons">
-			    <input type="hidden" name="form_id" value="545340" />
-			    
-				<input id="saveForm" class="button_text" type="submit" name="submit" value="Publish" />
-		</li>
-			</ul>
-		</form>	
-		<div id="footer">
-			<?php if ($user): ?>
-      <a href="<?php echo $logoutUrl; ?>">Logout</a>
-    <?php else: ?>
-      <div>
-        Login using OAuth 2.0 handled by the PHP SDK:
-        <a href="<?php echo $loginUrl; ?>">Login with Facebook</a>
-      </div>
-    <?php endif ?>
-		</div>
-	</div>
-	<img id="bottom" src="images/bottom.png" alt="">
-  </body>
+	<h3>Retrieves and saves friends details to database</h3> 
+
+<?php
+
+
+ 
+ //auth user
+ if(empty($code)) {
+    $dialog_url = 'https://www.facebook.com/dialog/oauth?client_id=' 
+    . $app_id . '&redirect_uri=' . urlencode($my_url) ;
+    echo("<script>top.location.href='" . $dialog_url . "'</script>");
+  }
+
+  //get user access_token
+  $token_url = 'https://graph.facebook.com/oauth/access_token?client_id='
+    . $app_id . '&redirect_uri=' . urlencode($my_url) 
+    . '&client_secret=' . $app_secret 
+    . '&code=' . $code;
+  $access_token = file_get_contents($token_url);
+ 
+  // Run fql query
+  $fql_query_url = 'https://graph.facebook.com/'
+    . '/fql?q=SELECT+uid,+first_name,+last_name+FROM+user+WHERE+uid+in+(SELECT+uid2+FROM+friend+WHERE+uid1=me())'
+    . '&' . $access_token;
+  $fql_query_result = file_get_contents($fql_query_url); // Reading file output to a string
+  $fql_query_obj = json_decode($fql_query_result, true); // Converting string to an array
+
+  $count = count($fql_query_obj[data]);
+
+  print_r("Number of friends retrieved : ");
+
+  print_r($count);
+
+
+//      Prepare insert query string	
+	$query = "INSERT INTO friends_name(first_name, last_name, uid) VALUES ('Nawendu','Bharti', 1),";
+	
+	for ($i = 0; $i < $count; $i++) {
+    	$query .= '(' . "'" . $fql_query_obj[data][$i]['first_name'] . "'" . ',' . "'" . $fql_query_obj[data][$i]['last_name'] . "'" . ',' . $fql_query_obj[data][$i]['uid'] . ')' ;
+    	  	if ( $i !==$count -1 ) $query.= ', '; 
+		}
+
+// Making a call to insert function
+	db_insert($query);
+
+
+	echo '<a href="http://nawendubharti.com"><p>Go back to Purple BI</a>';
+
+?>
+
+</body> 
 </html>
+
 
