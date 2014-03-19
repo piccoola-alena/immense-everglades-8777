bf96dcbb15a6c291a33178913a8ff086407b8783
piccoola-alena <piccoola3@mail.ru>
2014-03-19 21:55:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<html>
+<head></head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/ru_RU/all.js#xfbml=1&appId=1400569390162590";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+</body>
+</html>
+
+
 <?php
 header('Content-Type: text/html; charset=utf-8');
 setlocale(LC_ALL, 'ru_RU.65001', 'rus_RUS.65001', 'Russian_Russia. 65001', 'russian');
